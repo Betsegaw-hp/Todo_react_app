a51ac2a0509e3cc3583ce3078705065a0a110040
betsegaw-hp <betse53@gmail.com>
2021-02-10 00:07:35 -0800

    unsure about this one

---
+++ src/js/Components/Notification/PopUp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PopUp = () => {
+
+
+  return (
+    <div>
+      <div className="completed-tasks-container">
+    <h3 className="completed-task-title">
+      Action alert</h3>
+     
+      <span className="emptyText"> 
+      Nothing here. <span>But you can make one!</span></span>
+     </div>
+    </div>
+  )
+}
+
+export default PopUp;
