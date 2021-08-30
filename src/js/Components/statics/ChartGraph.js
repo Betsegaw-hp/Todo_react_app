3184a112bd6c2b7693c36c114559bfb89a6b4a47
betsegaw-hp <betse534@gmail.com>
2021-08-30 01:10:09 -0700

    simple change

--- src/js/Components/statics/ChartGraph.js
+++ src/js/Components/statics/ChartGraph.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ChartGraph = () => {
   return (
     <div className="chart-container">
-        <h1>I am the chart</h1>
+        <h1>Data visualization section</h1>
   </div>
   )
 }
