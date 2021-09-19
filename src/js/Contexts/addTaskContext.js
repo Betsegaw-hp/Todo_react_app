36d167e1560e5700dff68ff1cca85e00c87902f5
betsegaw-hp <betse53@gmail.com>
2021-09-19 21:35:35 +0300

    code refactore for DB requestes

--- src/js/Contexts/addTaskContext.js
+++ src/js/Contexts/addTaskContext.js
@@ -2,45 +2,9 @@ import React, { createContext, useEffect, useReducer } from 'react';
 import  useUpdateEffect  from '../CustomHooks/useUpdateEffect'
 import {  taskAdded, taskCompleted, taskFaild } from '../Reducers/actions';
 import TaskReducer from '../Reducers/TaskReducer';
+import { updateDB, read_DB } from '../DB/indexedDB';
 
 export const TaskContext = createContext();
-let db, DB_version = 1;
-
-function intArr(label) {
-  return new Promise((resolve, reject) => {
-    const DB = indexedDB.open(`HAPPY-TODO-App`, DB_version)
-    DB.onupgradeneeded = (e) => {
-      db = e.target.result
-  
-      const DBtasks = db.createObjectStore('Tasks', {keyPath: 'id'})
-      const DBcompletedTasks = db.createObjectStore('Completed_tasks', {keyPath: 'id'})
-      const DBfaildTasks = db.createObjectStore('Faild_tasks', {keyPath: 'id'})
-    
-      DBtasks.createIndex('id', 'id', { unique : true})
-      DBtasks.createIndex('title', 'title', { unique : false})
-      DBcompletedTasks.createIndex('id', 'id', { unique : true})
-      DBcompletedTasks.createIndex('title', 'title', { unique : false})
-      DBfaildTasks.createIndex('id', 'id', { unique : false})
-      DBfaildTasks.createIndex('title', 'title', { unique : false})
-    }
-    DB.onsuccess = () => {
-      db = DB.result
-      console.log('succes reading' , label)
-  
-      const tx = db.transaction(label)
-      const objectStore = tx.objectStore(label)
-      const req = objectStore.getAll()
-      req.onsuccess = (e) => {
-        // console.log(req.result)
-        resolve(req.result) 
-      } 
-    }
-    DB.onerror = (e) => {
-      db = e.target.result;
-      reject(db)
-    }
-  })
-}
 
 const AddTaskContextProvider = (props) => {
   const [tasks, dispatch] = useReducer(TaskReducer, []);
@@ -51,7 +15,7 @@ const AddTaskContextProvider = (props) => {
   useEffect(()=> {
     // Works but unstable // needs attention
     ['Tasks', 'Completed_tasks', 'Faild_tasks'].forEach((item, i) => {
-      intArr(item)
+      read_DB(item)
       .then(result =>{
          switch (i) {
            case 0:
@@ -82,68 +46,7 @@ const AddTaskContextProvider = (props) => {
 
 // no intial render
   useUpdateEffect(() => {
-    const DBopenReq = indexedDB.open(`HAPPY-TODO-App`, DB_version)
-  DBopenReq.onerror = (e) => {
-        //  on error
-        db = e.target.result;
-        
-        console.warn(db)
-      }
-  DBopenReq.onupgradeneeded = (e) => {
-    // on upgradeneeded
-    db = e.target.result;
-    db.onerror = function(event) {
-      alert(`Error loading database: ${event.target.error}`);
-  }
-    
-  // create objecetStore 
-
-  const DBtasks = db.createObjectStore('Tasks', {keyPath: 'id'})
-  const DBcompletedTasks = db.createObjectStore('Completed_tasks', {keyPath: 'id'})
-  const DBfaildTasks = db.createObjectStore('Faild_tasks', {keyPath: 'id'})
-
-  DBtasks.createIndex('id', 'id', { unique : true})
-  DBtasks.createIndex('title', 'title', { unique : false})
-  DBcompletedTasks.createIndex('id', 'id', { unique : true})
-  DBcompletedTasks.createIndex('title', 'title', { unique : false})
-  DBfaildTasks.createIndex('id', 'id', { unique : false})
-  DBfaildTasks.createIndex('title', 'title', { unique : false})
-  console.log('obJect Store created')
-} 
-   DBopenReq.onsuccess = e => {
-    db = DBopenReq.result;
-    console.log('success', db)
- 
-    addToDB('Tasks', tasks)
-    addToDB('Completed_tasks', completedTasks)
-    addToDB('Faild_tasks', faildTasks)
-  }
-
-  function addToDB(storeName , Item ) {
-    let tx = db.transaction(storeName, 'readwrite')
-
-    tx.oncomplete = e => {
-      console.log('tx completed', tx)
-    }
-    tx.onerror = e => {
-      console.log('tx on error', tx)
-    }
-
-    let objectStore = tx.objectStore(storeName), oSRequest;
-      //  clear the store
-    let oSclearReq = objectStore.clear();
-       oSclearReq.onsuccess = () => {
-         console.log('store cleared')
-       }
-     Item.forEach(task => {
-       oSRequest = objectStore.put(task);
-      oSRequest.onsuccess = e => {
-      console.log(task.id, 'added successfuly')
-    }
-    });
-    
-    
-  }
+    updateDB([tasks, completedTasks, faildTasks])
   }, [tasks, completedTasks, faildTasks])
  
 
--- src/js/DB/indexedDB.js
+++ src/js/DB/indexedDB.js
@@ -1,6 +1,6 @@
-// //  GLOBALS
-// const PREFIX = 'HAPPY-'
-// let db, DB_version = 1;
+//  GLOBALS
+const PREFIX = 'HAPPY-'
+let db, DB_version = 1;
 
 // window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
   
@@ -8,58 +8,108 @@
 
 // window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
 
-// if(!window.indexedDB) { return console.log('indexedDB not supported')}
 
-// class DB {
-//   constructor(DB_name, DB_version) {
-//     this.DB_name = DB_name
-//     this.DB_version = DB_version
-//   }
-//   get open() {
-//     return this.DB_open()
-//   }
-//   DB_open() {
-//     return new Promise(
-//       (resolve, reject) => {
-//         const DBopenReq = window.indexedDB.open(DB_name, DB_version)
-//         DBopenReq.onerror = (e) => {
-//               //  on error
-//               db = e.target.result;
-              
-//               reject(db)
-//             }
-//         DBopenReq.onupgradeneeded = (e) => {
-//           // on upgradeneeded
-//           db = e.target.result;
-//           db.onerror = function(event) {
-//             alert(`Error loading database: ${event.target.error}`);
-//             reject(event)
-//         }
-          
-//         // create objecetStore 
-      
-//         const DBtasks = db.createObjectStore('Tasks', {keyPath: 'id'})
-//         const DBcompletedTasks = db.createObjectStore('Completed_tasks', {keyPath: 'id'})
-//         const DBfaildTasks = db.createObjectStore('Faild_tasks', {keyPath: 'id'})
-      
-//         DBtasks.createIndex('id', 'id', { unique : true})
-//         DBtasks.createIndex('title', 'title', { unique : false})
-//         DBcompletedTasks.createIndex('id', 'id', { unique : true})
-//         DBcompletedTasks.createIndex('title', 'title', { unique : false})
-//         DBfaildTasks.createIndex('id', 'id', { unique : false})
-//         DBfaildTasks.createIndex('title', 'title', { unique : false})
-//         console.log('obJect Store created')
-//       } 
-//         DBopenReq.onsuccess = e => {
-//           db = DBopenReq.result;
-//           console.log('success', db)
-      
-//           // addToDB('Tasks', tasks)
-//           // addToDB('Completed_tasks', completedTasks)
-//           // addToDB('Faild_tasks', faildTasks)
-//         }
-//       }
-//     );
-//   };
 
-// }
+export function updateDB(tasks) {
+    if(!window.indexedDB) { return console.log('indexedDB not supported')}
+    const DBopenReq = window.indexedDB.open(`HAPPY-TODO-App`, DB_version)
+    DBopenReq.onerror = (e) => {
+            //  on error
+            db = e.target.result;
+            
+            console.warn(db)
+        }
+    DBopenReq.onupgradeneeded = (e) => {
+        // on upgradeneeded
+        db = e.target.result;
+        db.onerror = function(event) {
+        alert(`Error loading database: ${event.target.error}`);
+    }
+        
+    // create objecetStore 
+    
+    const DBtasks = db.createObjectStore('Tasks', {keyPath: 'id'})
+    const DBcompletedTasks = db.createObjectStore('Completed_tasks', {keyPath: 'id'})
+    const DBfaildTasks = db.createObjectStore('Faild_tasks', {keyPath: 'id'})
+    
+    DBtasks.createIndex('id', 'id', { unique : true})
+    DBtasks.createIndex('title', 'title', { unique : false})
+    DBcompletedTasks.createIndex('id', 'id', { unique : true})
+    DBcompletedTasks.createIndex('title', 'title', { unique : false})
+    DBfaildTasks.createIndex('id', 'id', { unique : false})
+    DBfaildTasks.createIndex('title', 'title', { unique : false})
+    console.log('obJect Store created')
+    } 
+        DBopenReq.onsuccess = e => {
+        db = DBopenReq.result;
+        console.log('success', db);
+
+        ['Tasks', 'Completed_tasks', 'Faild_tasks'].forEach((e,i) => {
+            addToDB(e, tasks[i] )
+        })
+    //   addToDB('Tasks', tasks)
+    //   addToDB('Completed_tasks', completedTasks)
+    //   addToDB('Faild_tasks', faildTasks)
+    }
+    
+    function addToDB(storeName , Item ) {
+        let tx = db.transaction(storeName, 'readwrite')
+    
+        tx.oncomplete = e => {
+        console.log('tx completed', tx)
+        }
+        tx.onerror = e => {
+        console.log('tx on error', tx)
+        }
+    
+        let objectStore = tx.objectStore(storeName), oSRequest;
+        //  clear the store
+        let oSclearReq = objectStore.clear();
+            oSclearReq.onsuccess = () => {
+            console.log('store cleared')
+            }
+        Item.forEach(task => {
+            oSRequest = objectStore.put(task);
+        oSRequest.onsuccess = e => {
+        console.log(task.id, 'added successfuly')
+        }
+        });
+        
+    }
+}
+
+export function read_DB(label) {
+    return new Promise((resolve, reject) => {
+      const DB = indexedDB.open(`HAPPY-TODO-App`, DB_version)
+      DB.onupgradeneeded = (e) => {
+        db = e.target.result
+    
+        const DBtasks = db.createObjectStore('Tasks', {keyPath: 'id'})
+        const DBcompletedTasks = db.createObjectStore('Completed_tasks', {keyPath: 'id'})
+        const DBfaildTasks = db.createObjectStore('Faild_tasks', {keyPath: 'id'})
+      
+        DBtasks.createIndex('id', 'id', { unique : true})
+        DBtasks.createIndex('title', 'title', { unique : false})
+        DBcompletedTasks.createIndex('id', 'id', { unique : true})
+        DBcompletedTasks.createIndex('title', 'title', { unique : false})
+        DBfaildTasks.createIndex('id', 'id', { unique : false})
+        DBfaildTasks.createIndex('title', 'title', { unique : false})
+      }
+      DB.onsuccess = () => {
+        db = DB.result
+        console.log('succes reading' , label)
+    
+        const tx = db.transaction(label)
+        const objectStore = tx.objectStore(label)
+        const req = objectStore.getAll()
+        req.onsuccess = (e) => {
+          // console.log(req.result)
+          resolve(req.result) 
+        } 
+      }
+      DB.onerror = (e) => {
+        db = e.target.result;
+        reject(db)
+      }
+    })
+  }
